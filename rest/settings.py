63a15762db20d441cf1e1b101ed44d5ff535ee13
pierre94 <xiongyongxin@hotmail.com>
2016-05-21 21:29:27 +0800

    set debug =false

--- rest/settings.py
+++ rest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8e3n286czx!!9wrzrw%r2k2$wupvhd8vbr=2%nfllsvqdhdp@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'ws.bear2.cn',
